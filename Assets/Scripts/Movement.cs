dd58c2076837b12ebd1f74e7cba17f1c4db1caa4
Olly <oliver.m.rea@gmail.com>
2021-05-03 13:37:21 +0100

    - Updated Ship design - Added ship exhaust on movement - Added Main Menu components - Added TextMesh Pro package to build

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,7 +12,7 @@ public class Movement : MonoBehaviour {
     private Vector2 velocity;
 
     // Speed modifiers (Constants)
-    private int MAX_SPEED = 12;
+    private int MAX_SPEED = 24;
     private double SPEED_FACTOR = 0.08;
 
     // Method called when class initialized
@@ -20,24 +20,33 @@ public class Movement : MonoBehaviour {
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
     
-    // Method to move the character
+    // Method to move the player model
     private void Move(Vector2 input) {
         // Get current rigidbody velocity
         velocity = rigidBody.velocity;
         
         // Set the rigidbody's new rotation
         rigidBody.rotation -= (float)(input.x * 0.7 * runspeed);
-
-        // Get the current vector magnitude  
+        // Get the current vector magnitude
         double magnitude = Math.Sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y));
+
+        // Show the ship exhaust (if input.y == 1 (up/forwards) OR the rounded absolute value of input.x == 0.7 (combo of up/forward and left/right))
+        if (input.y == 1 || Math.Abs(Math.Round(input.x, 1)) == 0.7) {
+            this.gameObject.transform.GetChild(1).GetComponent<Renderer>().enabled = true;
+        }
         // Change the player's velocity vectors (only if vector magnitude < MAX_SPEED AND input != -1 (down/backwards))
         if(input.y != -1) {
+            // Update the movement vector
             if (magnitude < MAX_SPEED){
                 velocity.x -= (float)(input.y * Math.Sin(rigidBody.rotation/57.2958) * SPEED_FACTOR) * runspeed; // SOH(CAH)TOA (Convert to radians)
                 velocity.y += (float)(input.y * Math.Cos(rigidBody.rotation/57.2958) * SPEED_FACTOR) * runspeed; // (SOH)CAHTOA (Convert to radians)
             }
             // Set the rigidbody's new velocity
             rigidBody.velocity = velocity;
+        }
+        // Hide the ship exhaust
+        if(input.y != 1 && Math.Abs(Math.Round(input.x, 1)) != 0.7){
+            this.gameObject.transform.GetChild(1).GetComponent<Renderer>().enabled = false;
         }
     }
 
